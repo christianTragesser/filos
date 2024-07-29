d34ad53de4490c8e45afbd30e4f3c60b8281b429
Christian Tragesser <christian@evoen.net>
2024-07-29 07:13:36 -0500

    migrate daas kubernetes resources to helm chart

--- install_poc.sh
+++ install_poc.sh
@@ -3,14 +3,14 @@
 printf "\nEnter the site FQDN: "
 read FQDN
 
-if kubectl get secret gptscript-key -n daas > /dev/null; then
-  printf "\n Found GPTScript APIkey secret.\n"
+if kubectl get secret gptscript-key -n daas 2>&1> /dev/null; then
+  printf "\nFound GPTScript APIkey secret.\n"
 else
   printf "\nEnter your OpenAI API key: "
   read -s OPENAI_API_KEY
   kubectl create namespace daas > /dev/null || true
   kubectl create secret generic gptscript-key -n daas \
-      --from-literal=key="${OPENAI_API_KEY}"
+      --from-literal=key="${OPENAI_API_KEY}" > /dev/null
 fi
 
 printf "\n"
@@ -32,11 +32,14 @@ helm install --namespace coroot --create-namespace --set node-agent.tracesEndpoi
     --set corootCE.ingress.className=nginx coroot coroot/coroot
 
 # daas
-printf "\nChecking for daas services:\n"
-kubectl apply -f ./manifests/daas
+printf "\nChecking for daas:\n"
+helm ls -n daas| grep daas > /dev/null || \
+helm install daas ./charts/daas -n daas --set fqdn=${FQDN}
 
+printf "\nPausing for 3 minutes to configure Coroot to send webhooks to daas.\n"
+sleep 180
 
-## myapp-mem
+# myapp-mem
 printf "\nChecking for myapp-mem:\n"
 helm ls | grep myapp-mem > /dev/null || \
 helm install myapp-mem ./charts/myapp -f ./charts/myapp/mem-values.yaml --set fqdn=${FQDN}
